c67ba505fca5ee6d600d0bc92cbc376dfe265665
PranavTiwari44 <tiwa0027@algonquinlive.com>
2021-11-08 01:08:04 -0500

    Auto sorted

---
+++ str_MrgV2.py
@@ -0,0 +1,10 @@
+str1 = "acegikmoqsuwy"
+str2 = "bdfhjlnprtvxz"
+
+merged_Str = str1 + str2
+
+auto_Sorted = sorted(merged_Str)
+merged_Str = "".join(auto_Sorted)
+
+print("The automatic sorted string is " + merged_Str)
+
