58d3bd26bddcf98d4ccb2f903e603aa9d5a2ed73
PranavTiwari44 <tiwa0027@algonquinlive.com>
2021-11-09 13:59:16 -0500

    guessing game with func or loop

--- guessing_GameV2.py
+++ guessing_GameV2.py
@@ -1,4 +1,19 @@
 answer = 5
 
+def guessingsys(numb):
+    if numb < answer:
+        print("Guess a higher number please!")
+    elif numb > answer:
+        print("Guess a lower number please!")
+    else:
+        print("You got it!")
+
+
 guess = int(input("Guess here: "))
+while guess == answer:
+    guessingsys(guess)
+
+
+
+
 
