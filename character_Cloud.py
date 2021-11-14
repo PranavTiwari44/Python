43c64605ba0e5913579218f6ca9863173a6bb01c
PranavTiwari44 <tiwa0027@algonquinlive.com>
2021-11-13 23:36:14 -0500

    character cloud program with advance pandas method

--- character_Cloud.py
+++ character_Cloud.py
@@ -1,8 +1,20 @@
-initialSample = "This past summer, I had the privilege of participating in the University of Notre Dame’s Research Experience for Undergraduates (REU) program ."
-
+import pandas
+initialSample = "This past summer, I had the privilege of participating in the University of Notre Dame's Research Experience for Undergraduates (REU) program ."
+str = ""
 
 for characters in initialSample:
-    print(characters)
+    if " " not in characters:
+        if "," not in characters:
+            if "'" not in characters:
+                if "." not in characters:
+                    if "(" not in characters:
+                        if ")" not in characters:
+                            str = str + characters
+
+
+print(pandas.Series(list(str.casefold())).value_counts())
+
+
 
 
 
