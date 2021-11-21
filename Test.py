d3e3b05a23b6b021f8658f02a797bb7ca68f5664
PranavTiwari44 <tiwa0027@algonquinlive.com>
2021-11-21 13:14:04 -0500

    Added basic character cloud graph with some edited result

--- Test.py
+++ Test.py
@@ -1,12 +1,38 @@
-print("Pepe")
-print("The student name is {0}".format("pranav"))
+#print("Pepe")
+#print("The student name is {0}".format("pranav"))
 
-for i in range(1, 11):
-    if i < 11:
-        {print("No. {0:2} squared is {1:3}".format(i, i ** 2))}
+#for i in range(1, 11):
+#    if i < 11:
+#        {print("No. {0:2} squared is {1:3}".format(i, i ** 2))}
 
-print("\n")
-print("Pi is {0:.50f}".format(22/7))
+#print("\n")
+#print("Pi is {0:.50f}".format(22/7))
 
-centerString = "center"
-print((centerString.center(100)).count(" "))
+#centerString = "center"
+#print((centerString.center(100)).count(" "))
+
+
+import matplotlib.pyplot as plt
+
+# x-coordinates of left sides of bars
+left = [1, 2, 3, 4, 5]
+
+# heights of bars
+height = [10, 24, 36, 40, 5]
+
+# labels for bars
+tick_label = ['one', 'two', 'three', 'four', 'five']
+
+# plotting a bar chart
+plt.bar(left, height, tick_label = tick_label,
+        width = 0.8, color = ['red', 'green'])
+
+# naming the x-axis
+plt.xlabel('x - axis')
+# naming the y-axis
+plt.ylabel('y - axis')
+# plot title
+plt.title('My bar chart!')
+
+# function to show the plot
+plt.show()
---
+++ character_Cloud_Graph.py
@@ -0,0 +1,36 @@
+import pandas
+import re
+import matplotlib.pyplot as plt
+
+initialSample = "This past summer, I had the privilege of participating in the University of Notre Dame's Research Experience for Undergraduates (REU) program ."
+strF = ""
+str = re.sub(r"[^a-zA-Z0-9]","",initialSample)
+
+for characters in str:
+    strF = str + characters
+
+str_Final = pandas.Series(list(strF.casefold())).value_counts()
+
+left = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# heights of bars
+height = str_Final[0:20]
+
+# labels for bars
+tick_label = ['E', 'R', 'I', 'T', 'A', 'S', 'P', 'N', 'U', 'H', 'O', 'G', 'D', 'M', 'F', 'C', 'V', 'L', 'Y', 'X']
+
+# plotting a bar chart
+plt.bar(left, height, tick_label = tick_label,
+        width = 0.8, color = ['red', 'green'])
+
+# naming the x-axis
+plt.xlabel('Characters')
+# naming the y-axis
+plt.ylabel('No. of character counts')
+# plot title
+plt.title('Character cloud!')
+
+# function to show the plot
+plt.show()
+
+
