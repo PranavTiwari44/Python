2b808ace50c6d0ddb0e2dee892d0fa2b5765fd4c
PranavTiwari44 <tiwa0027@algonquinlive.com>
2022-01-30 20:52:11 -0500

    Update guessing_Game_Points.py

--- guessing_Game_Points.py
+++ guessing_Game_Points.py
@@ -1,12 +1,17 @@
 import random
 
-highest = 20
+highest = 100
 answer = random.randint(1, highest)
+print(answer)
 count = 0
 
 numb = int(input("Guess a number between 1 - {}: ".format(highest)))
 while numb != answer:
-        numb = int(input("Guess here again: "))
+        numb = input("Guess here again: ")
+        while numb == '':
+            numb = input("Try Again, and re-enter: ")
+            continue
+        numb = int(numb)
         if numb == answer:
             continue
         else:
@@ -23,5 +28,7 @@ elif (highest * 0.75) > count >= (highest * 0.5):
     print("You scored 50 points out of 100: You can do better!")
 elif (highest * 0.5) > count >= (highest * 0.25):
     print("You scored 75 points out of 100: Good work!")
+elif 3 < count < (highest * 0.25):
+    print("You scored 90 points out of 100: Great work!")
 elif 3 > count:
     print("You scored 100 points out of 100: Awesome!")
