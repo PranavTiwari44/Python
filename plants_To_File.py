eda8f1ee009254192b0ca1edb521d7c616e10eef
PranavTiwari44 <tiwa0027@algonquinlive.com>
2022-04-22 19:17:32 -0400

    Tested how to redirect data and create a new text file in the process!

---
+++ plants_To_File.py
@@ -0,0 +1,31 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac- Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+plants_file = "flowers_print.text"
+#
+# with open(plants_file, "w") as plants:
+#     for plant in data:
+#         print(plant, file=plants)
+
+with open(plants_file, "w") as plants:
+    for plant in data:
+        plants.write(plant)
