2982a7f613df7a9ec788bee3938ab766886fab20
PranavTiwari44 <tiwa0027@algonquinlive.com>
2022-05-12 22:24:35 -0400

    Made a calculator gui using tkinter and completed the prev GUI

--- Tkinter/screen.py
+++ Tkinter/screen.py
@@ -5,15 +5,16 @@ mainWindow = tkinter.Tk()
 
 mainWindow.title("Grid Demo")
 mainWindow.geometry('640x480-8-200')
+mainWindow['padx'] = 8
 
 label = tkinter.Label(mainWindow, text="Tkinter Grid Demo")
 label.grid(row=0, column=0, columnspan=3)
 
-mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(0, weight=100)
 mainWindow.columnconfigure(1, weight=1)
-mainWindow.columnconfigure(2, weight=3)
-mainWindow.columnconfigure(3, weight=3)
-mainWindow.columnconfigure(4, weight=3)
+mainWindow.columnconfigure(2, weight=1000)
+mainWindow.columnconfigure(3, weight=600)
+mainWindow.columnconfigure(4, weight=1000)
 mainWindow.rowconfigure(0, weight=1)
 mainWindow.rowconfigure(1, weight=10)
 mainWindow.rowconfigure(2, weight=1)
@@ -27,7 +28,7 @@ for zone in os.listdir('/Windows/System32'):
     fileList.insert(tkinter.END, zone)
 
 listScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=fileList.yview)
-listScroll.grid(row=1, column=1, sticky='nsew', rowspan=2)
+listScroll.grid(row=1, column=1, sticky='nsw', rowspan=2)
 fileList['yscrollcommand'] = listScroll.set
 
 optionFrame = tkinter.LabelFrame(mainWindow, text='File Details')
@@ -60,4 +61,25 @@ tkinter.Label(timeFrame, text=':').grid(row=0, column=3)
 secondSpinner.grid(row=0, column=4)
 timeFrame['padx'] = 36
 
+dateFrame = tkinter.Frame(mainWindow)
+dateFrame.grid(row=4, column=0, sticky='new')
+dayLabel = tkinter.Label(dateFrame, text="Day")
+monthLabel = tkinter.Label(dateFrame, text="Month")
+yearLabel = tkinter.Label(dateFrame, text="Year")
+dayLabel.grid(row=0, column=0, sticky='w')
+monthLabel.grid(row=0, column=1, sticky='w')
+yearLabel.grid(row=0, column=2, sticky='w')
+daySpin = tkinter.Spinbox(dateFrame, width=5, from_=1, to=31)
+monthSpin = tkinter.Spinbox(dateFrame, width=5, values=("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+                                                        "Oct", "Nov", "Dec"))
+yearSpin = tkinter.Spinbox(dateFrame, width=5, from_=2000, to=2099)
+daySpin.grid(row=1, column=0)
+monthSpin.grid(row=1, column=1)
+yearSpin.grid(row=1, column=2)
+dateFrame['padx'] = 36
+
+okButton = tkinter.Button(mainWindow, text='OK')
+cancelButton = tkinter.Button(mainWindow, text='Cancel', command=mainWindow.quit)
+okButton.grid(row=4, column=3, sticky='e')
+cancelButton.grid(row=4, column=4, sticky='w')
 mainWindow.mainloop()
